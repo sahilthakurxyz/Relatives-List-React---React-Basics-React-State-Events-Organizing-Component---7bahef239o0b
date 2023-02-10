fc602ddf8030953cbb1b730231731a3040c08ffe
Sahil <sahilthakurxyz@gmail.com>
2023-02-11 02:01:10 +0530

    re-update

--- src/components/App.js
+++ src/components/App.js
@@ -3,21 +3,27 @@ import "../styles/App.css";
 let Global_count = 0;
 function App() {
   let relativeList = [
-    "relativeListItem1",
-    "relativeListItem2",
-    "relativeListItem3",
-    "relativeListItem4",
+    {
+      relativeListItem: "relativeListItem1",
+      relativeName: "Bhubesh",
+    },
+    {
+      relativeListItem: "relativeListItem2",
+      relativeName: "Rohit",
+    },
+    {
+      relativeListItem: "relativeListItem3",
+      relativeName: "Nariender",
+    },
   ];
 
   let i = 0;
   return (
     <div id="main">
       <ol key={Global_count++}>
-        <li key={relativeList[0]}>Bhubesh</li>
-        <li key={relativeList[1]}>Nariender</li>
-        <li key={relativeList[2]}>Rohit</li>
-        <li key={relativeList[3]}>Ashish</li>
-        <li key={relativeList[4]}>Nitish</li>
+        {relativeList.map((relative) => (
+          <li key={relative.relativeListItem}>{relative.relativeName}</li>
+        ))}
       </ol>
     </div>
   );
