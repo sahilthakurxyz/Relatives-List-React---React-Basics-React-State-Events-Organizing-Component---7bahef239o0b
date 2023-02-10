9a12ebfda2f59b4025fd4d746289a2cc2fd34d0e
Sahil <sahilthakurxyz@gmail.com>
2023-02-11 00:53:42 +0530

    update

--- src/components/App.js
+++ src/components/App.js
@@ -1,16 +1,24 @@
-import React, {Component, useState} from "react";
-import '../styles/App.css';
+import React, { Component, useState } from "react";
+import "../styles/App.css";
 
-class App extends Component {
-    render() {
-
-        return(
-            <div id="main">
-               {/* Do not remove the main div */}
-            </div>
-        )
-    }
+function App() {
+  let relativeList = [
+    "relativeListItem1",
+    "relativeListItem2",
+    "relativeListItem3",
+    "relativeListItem4",
+  ];
+  return (
+    <div id="main">
+      <ol>
+        <li key={relativeList[0]}>Bhubesh</li>
+        <li key={relativeList[1]}>Nariender</li>
+        <li key={relativeList[2]}>Rohit</li>
+        <li key={relativeList[3]}>Ashish</li>
+        <li key={relativeList[4]}>Nitish</li>
+      </ol>
+    </div>
+  );
 }
-
 
 export default App;
