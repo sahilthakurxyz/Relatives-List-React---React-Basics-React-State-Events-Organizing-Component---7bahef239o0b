4557b4dd4f4f27c4b09e12d123bfa4a57f37ec0d
Sahil <sahilthakurxyz@gmail.com>
2023-02-11 03:04:52 +0530

    up

--- src/components/App.js
+++ src/components/App.js
@@ -2,29 +2,21 @@ import React, { Component, useState } from "react";
 import "../styles/App.css";
 
 const App = () => {
-  let relativeList = [
-    {
-      relativeListItem: "relativeListItem1",
-      relativeName: "Bhubesh",
-    },
-    {
-      relativeListItem: "relativeListItem2",
-      relativeName: "Rohit",
-    },
-    {
-      relativeListItem: "relativeListItem3",
-      relativeName: "Nariender",
-    },
-  ];
+  let relativeList = {
+    relativeListItem1: "Bhubesh",
+    relativeListItem2: "Rohit",
+    relativeListItem3: "Nariender",
+    relativeListItem4: "Ashish",
+    relativeListItem3: "Sachin",
+  };
 
-  let i = 0;
   return (
     <div id="main">
-      {relativeList.map((relative) => (
-        <ol key={relative.relativeName}>
-          <li key={relative.relativeListItem}>{relative.relativeName}</li>
-        </ol>
-      ))}
+      <ol>
+        {Object.keys(relativeList).map((keys) => (
+          <li key={keys}>{relativeList[keys]}</li>
+        ))}
+      </ol>
     </div>
   );
 };
